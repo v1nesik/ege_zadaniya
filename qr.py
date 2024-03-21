2821e6acfd58896b46585a04b23b81d5ba9de219
v1nesik <161463612+v1nesik@users.noreply.github.com>
2024-03-21 15:12:32 +0500

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,4 +1,37 @@
 import qrcode
-img = qrcode.make(input('Enter URL:'))
-type(img)  # qrcode.image.pil.PilImage
-img.save(input('Saving name(end .png): '))
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+root.geometry('300x250')
+root.title('QRCode generation')
+root.resizable(False, False)
+
+
+def generate():
+    img = qrcode.make(entry.get())
+    type(img)
+    img.save(entry_file.get())
+
+
+button = ttk.Button(root, text='Generate QR Code', command=generate)
+button.pack(side='bottom')
+
+label = ttk.Label(root, text='Enter URL')
+label.pack()
+
+entry = ttk.Entry(root, text='Enter URL')
+entry.pack()
+
+label_file = ttk.Label(root, text='Enter File Name(.png)')
+label_file.pack()
+
+entry_file = ttk.Entry(root, text='Enter File Name')
+entry_file.pack()
+
+label_help = ttk.Label(
+    root, text='The file is saved in the directory where you ran the code')
+label_help.pack()
+
+root.mainloop()
